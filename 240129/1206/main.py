0fe5f3b23c14440770cc5e0d68635f45f7983939
kaxadlec <kaxadlec@gmail.com>
2024-01-30 00:09:32 +0900

    update

--- 240129/1206/main.py
+++ 240129/1206/main.py
@@ -1,16 +1,22 @@
 
-T = int(input())
+T = 10
 
 for t in range(T):
      N = int(input())
-     arr = list(map(int, input().split()))
-
-     max_i = 0
-     for i in range(N-2):
+     height = list(map(int, input().split()))
+     result = []
+     res_sum = 0
+     for i in range(2, N-2):
+         around_max = 0
+         cnt = 0
+         around_list = []
          for j in range(i-2, i+3):
-             if arr[i] > arr[j]:
-                 result = arr[i] - arr[j]
-                 if max_i < result:
-                     max_i = result
+             if height[i] > height[j]:
+                cnt = cnt + 1
+                around_list.append(height[j])
+         if cnt == 4:
+             around_max = max(around_list)
+             res = height[i] - around_max
+             res_sum += res
 
-     print(max_i)
+     print(f'#{t+1} {res_sum}')
