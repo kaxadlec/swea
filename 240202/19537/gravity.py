b47b9d822b891a891f3a5f2adc3b840f4717152b
kaxadlec <kaxadlec@gmail.com>
2024-02-04 16:24:44 +0900

    Dd

--- 240202/19537/gravity.py
+++ 240202/19537/gravity.py
@@ -3,7 +3,13 @@ T = int(input())
 for tc in range(T):
     N = int(input())
     arr = list(map(int, input().split()))
-    for index in range(N):
+    max_fallen = 0
+    for i in range(N-1):
+        cnt = 0
+        for j in range(i+1, N):
+            if arr[i] > arr[j]:
+                cnt = cnt + 1
+        if max_fallen < cnt:
+            max_fallen = cnt
 
-
-    print(max_cnt)
+    print(f"#{tc+1} {max_fallen}")
