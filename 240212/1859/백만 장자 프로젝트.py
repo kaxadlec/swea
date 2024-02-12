c7d86b2328585eb9b6598f39bd5ead74267f9eca
kaxadlec <kaxadlec@gmail.com>
2024-02-12 20:14:48 +0900

    ídd

---
+++ 240212/1859/백만 장자 프로젝트.py
@@ -0,0 +1,9 @@
+T = int(input())
+for tc in range(T):
+    N = int(input())
+    prace_of_trade = list(map(int, input().split()))
+    result = 0
+
+
+    print(f'#{tc+1} {result}')
+    print(prace_of_trade)
