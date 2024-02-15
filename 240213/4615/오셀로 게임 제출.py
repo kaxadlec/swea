945d44b63308d6ab2ec1cba4493a13d7a59d383c
kaxadlec <kaxadlec@gmail.com>
2024-02-16 00:17:45 +0900

    othello complete

--- 240213/4615/오셀로 게임 제출.py
+++ 240213/4615/오셀로 게임 제출.py
@@ -7,8 +7,6 @@ def othello(i, j, bw, board):
         stack.append((ni, nj))
         while stack:
             si, sj = stack[-1]
-            print('stack', stack)
-            print('si sj', si, sj)
             if bw == 1 and 1 <= si < (N + 1) and 1 <= sj < (N + 1) and board[si][sj] != 0:
                 if board[si][sj] == 2:
                     ni = si + di[k]
@@ -34,7 +32,6 @@ def othello(i, j, bw, board):
                     break
 
             else:
-                print("해당사항 없음")
                 while stack:
                     stack.pop()
                 break
@@ -44,6 +41,8 @@ T = int(input())
 for tc in range(T):
     N, M = map(int, input().split())    # 보드 한변의 길이 N, 플레이어가 돌을 놓는 횟수 M
     board = [[0]*(N+1) for _ in range(N+1)]
+
+    # 초기 바둑판 상태
     start_i, start_j = int(N//2), int(N//2)
     start_di, start_dj = [0, 1, 1, 0], [0, 0, 1, 1]
     for k in range(4):
@@ -51,21 +50,17 @@ for tc in range(T):
             board[start_i+start_di[k]][start_j+start_dj[k]] = 2
         else:
             board[start_i+start_di[k]][start_j+start_dj[k]] = 1
+
     for _ in range(M):
         j, i, bw = (input().split())  # bw 1: 흑, 2: 백
         i, j, bw = int(i), int(j), int(bw)
         board[i][j] = bw
-
-        print()
-        print('i j bw', i, j, bw)
         othello(i, j, bw, board)
 
-        # print(board)
+    result_black, result_white = 0, 0
+    for bo in board:
+        row = ''.join(str(i) for i in bo)
+        result_black += row.count('1')
+        result_white += row.count('2')
 
-        for idx in board:
-            result = ''.join(str(i) for i in idx)
-            print(result)
-
-    result = 0
-
-    print(f'#{tc+1} {result}')
+    print(f'#{tc+1} {result_black} {result_white}')
