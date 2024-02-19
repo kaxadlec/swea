72b1bd946e2a1884a6467ac9a96b9f1c14f77abe
kaxadlec <kaxadlec@gmail.com>
2024-02-19 17:49:41 +0900

    dd

--- 240214/4880/토너먼트 카드게임.py
+++ 240214/4880/토너먼트 카드게임.py
@@ -1,19 +1,22 @@
 T = int(input())
 for tc in range(T):
     N = int(input())    # 인원수
-    cards = list(map(int, input().split()))
+    arr = ['0']
+    cards = arr.append(input())
+
+
     # 1: 가위, 2: 바위, 3: 보
     i = 1
     j = N
+    #
+    # for idx in range(i-1, ((i+j) // 2)):
+    #     if cards[idx] == cards[idx+1]
+    #     # print('1111', cards[idx])
+    # for idx in range(((i+j) // 2 + 1), (N+1)):
+    #     # print('2222', cards[idx-1])
+    #
 
-    for idx in range(i-1, ((i+j) // 2)):
-        if cards[idx]
-        # print('1111', cards[idx])
-    for idx in range(((i+j) // 2 + 1), (N+1)):
-        # print('2222', cards[idx-1])
-
-
-
+    print(arr)
     print(f'#{tc+1} ')
-    print(cards)
+
 
